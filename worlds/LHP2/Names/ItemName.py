0f8d7f9ae5fd500559c10ed0f48c21d432205f09
jrad <215300829+jradcode23@users.noreply.github.com>
2025-11-04 23:32:28 -0700

    Initial Commit, started setting up Names

---
+++ worlds/LHP2/Names/ItemName.py
@@ -0,0 +1,71 @@
+# Character Definitions
+
+# Character Token Definitions
+
+# Spell & Ability Definitions
+
+# Joke Spell Definitions
+
+# Level Unlock Definitions
+dt_unlock = "Dark Times: Level Unlocked"
+da_unlock = "Dumbledore's Army: Level Unlocked"
+foc_unlock = "Focus!: Level Unlocked"
+kd_unlock = "Kreacher Discomforts: Level Unlocked"
+agv_unlock = "A Giant Virtuoso: Level Unlocked"
+avt_unlock = "A Veiled Threat: Level Unlocked"
+oor_unlock = "Out of Retirement: Level Unlocked"
+jd_unlock = "Just Desserts: Level Unlocked"
+ansmc_unlock = "A Not So Merry Christmas: Level Unlocked"
+lh_unlock = "Love Hurts: Level Unlocked"
+ff_unlock = "Felix Felicis: Level Unlocked"
+thath_unlock = "The Horcrux and the Hand: Level Unlocked"
+tsh_unlock = "The Seven Harrys: Level Unlocked"
+mim_unlock = "Magic is Might: Level Unlocked"
+igd_unlock = "In Grave Danger: Level Unlocked"
+sal_unlock = "Sword and Locket: Level Unlocked"
+ll_unlock = "Lovegood's Lunacy: Level Unlocked"
+dob_unlock = "DOBBY!: Level Unlocked"
+ttd_unlock = "The Thief's Downfall: Level Unlocked"
+bts_unlock = "Back to School: Level Unlocked"
+bb_unlock = "Burning Bridges: Level Unlocked"
+fiend_unlock = "Fiendfyre Frenzy: Level Unlocked"
+st_unlock = "Snape's Tears: Level Unlocked"
+tfitp_unlock = "The Flaw in the Plan: Level Unlocked"
+
+# Student In Peril Definitions
+dt_sip = "Dark Times: Student in Peril"
+da_sip = "Dumbledore's Army: Student in Peril"
+foc_sip = "Focus!: Student in Peril"
+kd_sip = "Kreacher Discomforts: Student in Peril"
+agv_sip = "A Giant Virtuoso: Student in Peril"
+avt_sip = "A Veiled Threat: Student in Peril"
+oor_sip = "Out of Retirement: Student in Peril"
+jd_sip = "Just Desserts: Student in Peril"
+ansmc_sip = "A Not So Merry Christmas: Student in Peril"
+lh_sip = "Love Hurts: Student in Peril"
+ff_sip = "Felix Felicis: Student in Peril"
+thath_sip = "The Horcrux and the Hand: Student in Peril"
+tsh_sip = "The Seven Harrys: Student in Peril"
+mim_sip = "Magic is Might: Student in Peril"
+igd_sip = "In Grave Danger: Student in Peril"
+sal_sip = "Sword and Locket: Student in Peril"
+ll_sip = "Lovegood's Lunacy: Student in Peril"
+dob_sip = "DOBBY!: Student in Peril"
+ttd_sip = "The Thief's Downfall: Student in Peril"
+bts_sip = "Back to School: Student in Peril"
+bb_sip = "Burning Bridges: Student in Peril"
+fiend_sip = "Fiendfyre Frenzy: Student in Peril"
+st_sip = "Snape's Tears: Student in Peril"
+tfitp_sip = "The Flaw in the Plan: Student in Peril"
+
+# House Crest Definitions
+
+# Gold Brick Definitions
+
+# Gold Brick Purchase Definitions
+
+# Red Brick Definitions
+
+# Red Brick Purchase Definitions
+
+# True Status Definitions
---
+++ worlds/LHP2/Names/LocationName.py
@@ -0,0 +1,71 @@
+# Character Definitions
+
+# Character Token Definitions
+
+# Spell & Ability Definitions
+
+# Joke Spell Definitions
+
+# Level Unlock Definitions
+dt_beat = "Dark Times: Level Beaten"
+da_beat = "Dumbledore's Army: Level Beaten"
+foc_beat = "Focus!: Level Beaten"
+kd_beat = "Kreacher Discomforts: Level Beaten"
+agv_beat = "A Giant Virtuoso: Level Beaten"
+avt_beat = "A Veiled Threat: Level Beaten"
+oor_beat = "Out of Retirement: Level Beaten"
+jd_beat = "Just Desserts: Level Beaten"
+ansmc_beat = "A Not So Merry Christmas: Level Beaten"
+lh_beat = "Love Hurts: Level Beaten"
+ff_beat = "Felix Felicis: Level Beaten"
+thath_beat = "The Horcrux and the Hand: Level Beaten"
+tsh_beat = "The Seven Harrys: Level Beaten"
+mim_beat = "Magic is Might: Level Beaten"
+igd_beat = "In Grave Danger: Level Beaten"
+sal_beat = "Sword and Locket: Level Beaten"
+ll_beat = "Lovegood's Lunacy: Level Beaten"
+dob_beat = "DOBBY!: Level Beaten"
+ttd_beat = "The Thief's Downfall: Level Beaten"
+bts_beat = "Back to School: Level Beaten"
+bb_beat = "Burning Bridges: Level Beaten"
+fiend_beat = "Fiendfyre Frenzy: Level Beaten"
+st_beat = "Snape's Tears: Level Beaten"
+tfitp_beat = "The Flaw in the Plan: Level Beaten"
+
+# Student In Peril Definitions
+dt_sip = "Dark Times: Student in Peril"
+da_sip = "Dumbledore's Army: Student in Peril"
+foc_sip = "Focus!: Student in Peril"
+kd_sip = "Kreacher Discomforts: Student in Peril"
+agv_sip = "A Giant Virtuoso: Student in Peril"
+avt_sip = "A Veiled Threat: Student in Peril"
+oor_sip = "Out of Retirement: Student in Peril"
+jd_sip = "Just Desserts: Student in Peril"
+ansmc_sip = "A Not So Merry Christmas: Student in Peril"
+lh_sip = "Love Hurts: Student in Peril"
+ff_sip = "Felix Felicis: Student in Peril"
+thath_sip = "The Horcrux and the Hand: Student in Peril"
+tsh_sip = "The Seven Harrys: Student in Peril"
+mim_sip = "Magic is Might: Student in Peril"
+igd_sip = "In Grave Danger: Student in Peril"
+sal_sip = "Sword and Locket: Student in Peril"
+ll_sip = "Lovegood's Lunacy: Student in Peril"
+dob_sip = "DOBBY!: Student in Peril"
+ttd_sip = "The Thief's Downfall: Student in Peril"
+bts_sip = "Back to School: Student in Peril"
+bb_sip = "Burning Bridges: Student in Peril"
+fiend_sip = "Fiendfyre Frenzy: Student in Peril"
+st_sip = "Snape's Tears: Student in Peril"
+tfitp_sip = "The Flaw in the Plan: Student in Peril"
+
+# House Crest Definitions
+
+# Gold Brick Definitions
+
+# Gold Brick Purchase Definitions
+
+# Red Brick Definitions
+
+# Red Brick Purchase Definitions
+
+# True Status Definitions

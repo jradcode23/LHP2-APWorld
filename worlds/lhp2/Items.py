288ee3596c80fd53a1769e28b2b9e83c1fd12cba
jrad <215300829+jradcode23@users.noreply.github.com>
2025-11-23 21:32:39 -0700

    Add house crests

--- worlds/lhp2/Items.py
+++ worlds/lhp2/Items.py
@@ -15,7 +15,7 @@ class LHP2Item(Item):
 class LHP2ItemData(NamedTuple):
     code: Optional[int] = None
     qty: int = 0
-    classification: ItemClassification = ItemClassification.progression_deprioritized_skip_balancing
+    classification: ItemClassification = ItemClassification.filler
    
 
 base_item_id = 400000
@@ -60,15 +60,119 @@ level_unlock_loc_table: Dict[str, LHP2ItemData] = {
     ItemName.tfitp_unlock: LHP2ItemData(base_item_id + 473, ItemClassification.progression),
 }
 
-# 475 - 435 (to verify totals since there are extras)
+# 475 - 535 (to verify totals since there are extras)
 sip_item_table: Dict[str, LHP2ItemData] = {
     ItemName.sip: LHP2ItemData(base_item_id + 475, 24)
 }
 
+# 550 - 525
+house_crest_loc_table: Dict[str, LHP2ItemData] = {
+    # Gryff Crests
+    ItemName.dt_gc: LHP2ItemData(base_item_id + 550),
+    ItemName.da_gc: LHP2ItemData(base_item_id + 551),
+    ItemName.foc_gc: LHP2ItemData(base_item_id + 552),
+    ItemName.kd_gc: LHP2ItemData(base_item_id + 553),
+    ItemName.agv_gc: LHP2ItemData(base_item_id + 554),
+    ItemName.avt_gc: LHP2ItemData(base_item_id + 555),
+    ItemName.oor_gc: LHP2ItemData(base_item_id + 556),
+    ItemName.jd_gc: LHP2ItemData(base_item_id + 557),
+    ItemName.ansmc_gc: LHP2ItemData(base_item_id + 558),
+    ItemName.lh_gc: LHP2ItemData(base_item_id + 559),
+    ItemName.ff_gc: LHP2ItemData(base_item_id + 560),
+    ItemName.thath_gc: LHP2ItemData(base_item_id + 561),
+    ItemName.tsh_gc: LHP2ItemData(base_item_id + 562),
+    ItemName.mim_gc: LHP2ItemData(base_item_id + 563),
+    ItemName.igd_gc: LHP2ItemData(base_item_id + 564),
+    ItemName.sal_gc: LHP2ItemData(base_item_id + 565),
+    ItemName.ll_gc: LHP2ItemData(base_item_id + 566),
+    ItemName.dob_gc: LHP2ItemData(base_item_id + 567),
+    ItemName.ttd_gc: LHP2ItemData(base_item_id + 568),
+    ItemName.bts_gc: LHP2ItemData(base_item_id + 569),
+    ItemName.bb_gc: LHP2ItemData(base_item_id + 570),
+    ItemName.fiend_gc: LHP2ItemData(base_item_id + 571),
+    ItemName.st_gc: LHP2ItemData(base_item_id + 572),
+    ItemName.tfitp_gc: LHP2ItemData(base_item_id + 573),
+    # Slyth Crests
+    ItemName.dt_sc: LHP2ItemData(base_item_id + 574),
+    ItemName.da_sc: LHP2ItemData(base_item_id + 575),
+    ItemName.foc_sc: LHP2ItemData(base_item_id + 576),
+    ItemName.kd_sc: LHP2ItemData(base_item_id + 577),
+    ItemName.agv_sc: LHP2ItemData(base_item_id + 578),
+    ItemName.avt_sc: LHP2ItemData(base_item_id + 579),
+    ItemName.oor_sc: LHP2ItemData(base_item_id + 580),
+    ItemName.jd_sc: LHP2ItemData(base_item_id + 581),
+    ItemName.ansmc_sc: LHP2ItemData(base_item_id + 582),
+    ItemName.lh_sc: LHP2ItemData(base_item_id + 583),
+    ItemName.ff_sc: LHP2ItemData(base_item_id + 584),
+    ItemName.thath_sc: LHP2ItemData(base_item_id + 585),
+    ItemName.tsh_sc: LHP2ItemData(base_item_id + 586),
+    ItemName.mim_sc: LHP2ItemData(base_item_id + 587),
+    ItemName.igd_sc: LHP2ItemData(base_item_id + 588),
+    ItemName.sal_sc: LHP2ItemData(base_item_id + 589),
+    ItemName.ll_sc: LHP2ItemData(base_item_id + 590),
+    ItemName.dob_sc: LHP2ItemData(base_item_id + 591),
+    ItemName.ttd_sc: LHP2ItemData(base_item_id + 592),
+    ItemName.bts_sc: LHP2ItemData(base_item_id + 593),
+    ItemName.bb_sc: LHP2ItemData(base_item_id + 594),
+    ItemName.fiend_sc: LHP2ItemData(base_item_id + 595),
+    ItemName.st_sc: LHP2ItemData(base_item_id + 596),
+    ItemName.tfitp_sc: LHP2ItemData(base_item_id + 597),
+    # Raven Crests
+    ItemName.dt_rc: LHP2ItemData(base_item_id + 598),
+    ItemName.da_rc: LHP2ItemData(base_item_id + 599),
+    ItemName.foc_rc: LHP2ItemData(base_item_id + 600),
+    ItemName.kd_rc: LHP2ItemData(base_item_id + 601),
+    ItemName.agv_rc: LHP2ItemData(base_item_id + 602),
+    ItemName.avt_rc: LHP2ItemData(base_item_id + 603),
+    ItemName.oor_rc: LHP2ItemData(base_item_id + 604),
+    ItemName.jd_rc: LHP2ItemData(base_item_id + 605),
+    ItemName.ansmc_rc: LHP2ItemData(base_item_id + 606),
+    ItemName.lh_rc: LHP2ItemData(base_item_id + 607),
+    ItemName.ff_rc: LHP2ItemData(base_item_id + 608),
+    ItemName.thath_rc: LHP2ItemData(base_item_id + 609),
+    ItemName.tsh_rc: LHP2ItemData(base_item_id + 610),
+    ItemName.mim_rc: LHP2ItemData(base_item_id + 611),
+    ItemName.igd_rc: LHP2ItemData(base_item_id + 612),
+    ItemName.sal_rc: LHP2ItemData(base_item_id + 613),
+    ItemName.ll_rc: LHP2ItemData(base_item_id + 614),
+    ItemName.dob_rc: LHP2ItemData(base_item_id + 615),
+    ItemName.ttd_rc: LHP2ItemData(base_item_id + 616),
+    ItemName.bts_rc: LHP2ItemData(base_item_id + 617),
+    ItemName.bb_rc: LHP2ItemData(base_item_id + 618),
+    ItemName.fiend_rc: LHP2ItemData(base_item_id + 619),
+    ItemName.st_rc: LHP2ItemData(base_item_id + 620),
+    ItemName.tfitp_rc: LHP2ItemData(base_item_id + 621),
+    # Huffle Crests
+    ItemName.dt_hc: LHP2ItemData(base_item_id + 622),
+    ItemName.da_hc: LHP2ItemData(base_item_id + 623),
+    ItemName.foc_hc: LHP2ItemData(base_item_id + 624),
+    ItemName.kd_hc: LHP2ItemData(base_item_id + 625),
+    ItemName.agv_hc: LHP2ItemData(base_item_id + 626),
+    ItemName.avt_hc: LHP2ItemData(base_item_id + 627),
+    ItemName.oor_hc: LHP2ItemData(base_item_id + 628),
+    ItemName.jd_hc: LHP2ItemData(base_item_id + 629),
+    ItemName.ansmc_hc: LHP2ItemData(base_item_id + 630),
+    ItemName.lh_hc: LHP2ItemData(base_item_id + 631),
+    ItemName.ff_hc: LHP2ItemData(base_item_id + 632),
+    ItemName.thath_hc: LHP2ItemData(base_item_id + 633),
+    ItemName.tsh_hc: LHP2ItemData(base_item_id + 634),
+    ItemName.mim_hc: LHP2ItemData(base_item_id + 635),
+    ItemName.igd_hc: LHP2ItemData(base_item_id + 636),
+    ItemName.sal_hc: LHP2ItemData(base_item_id + 637),
+    ItemName.ll_hc: LHP2ItemData(base_item_id + 638),
+    ItemName.dob_hc: LHP2ItemData(base_item_id + 639),
+    ItemName.ttd_hc: LHP2ItemData(base_item_id + 640),
+    ItemName.bts_hc: LHP2ItemData(base_item_id + 641),
+    ItemName.bb_hc: LHP2ItemData(base_item_id + 642),
+    ItemName.fiend_hc: LHP2ItemData(base_item_id + 643),
+    ItemName.st_hc: LHP2ItemData(base_item_id + 644),
+    ItemName.tfitp_hc: LHP2ItemData(base_item_id + 645),
+}
 
 item_data_table = {
     **level_unlock_loc_table,
     **sip_item_table,
+    **house_crest_loc_table,
 }
 
 

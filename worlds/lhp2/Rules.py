30ee44b682c3942214a353a39867a018d2ace4c4
jrad <215300829+jradcode23@users.noreply.github.com>
2026-05-09 19:20:31 -0700

    Make it so starting inventory isn't duplicated in the multiworld

--- worlds/lhp2/Rules.py
+++ worlds/lhp2/Rules.py
@@ -201,7 +201,8 @@ can_get_herm_gray_coat = HasAll(itm.herm_bag_unlock, itm.specs_unlock)
 
 # Lovegood's Lunacy Logic
 can_access_ll_free = HasAll(itm.agua_unlock, itm.herm_bag_unlock)
-can_access_lunas_room = Or(char_is_strong_level, HasAll(itm.specs_unlock, itm.diffindo_unlock, itm.reducto_unlock))
+can_access_second_floor = Has(itm.diffindo_unlock)
+can_access_lunas_room = Or(char_is_strong_level, HasAll(itm.specs_unlock, itm.reducto_unlock)) & can_access_second_floor
 can_beat_ll = can_access_lunas_room & Has(itm.reducto_unlock)
 can_get_ll_rc = can_access_lunas_room & Has(itm.delum_unlock)
 can_get_ll_hc = can_use_spanner
